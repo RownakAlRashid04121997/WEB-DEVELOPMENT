759f5871cd42c1368f49b318aa1928025a402f9c
SunanRashidAIUB <sunanrashid@gmail.com>
2022-05-29 04:28:10 +0600

    Made some changes in the files

--- Web development/Javascript/loops.js
+++ Web development/Javascript/loops.js
@@ -1,3 +1,32 @@
-for(let i=0;i<10;i++){
+for(let i=0;i<11;i++){    /*for loop*/
     console.log("Hello World")
 }
+
+for(let j=50; j>4; j=j-5){  /*Reverse looping*/
+    console.log(j)
+}
+
+
+for(let i=0;i<11;i++){
+    if(i%3==0){  /*Basic if statement*/
+        console.log(i+"is divided by 3")
+    }
+}
+
+let i=0;
+while(i<25){ /*Basic While loop*/
+    console.log("While loop running ")
+    i++
+}
+
+
+let j=5;
+do{      /*In the do section the editor will do the job*/
+    console.log('Value of j is'+j)
+    j=j+5
+}while(j<25) /*Then inthe while section it will check wheither the conditon matches to continue*/
+
+
+
+
+
