2d1506a4a85aa248eab3689b58f8d10a52dfd876
SunanRashidAIUB <sunanrashid@gmail.com>
2022-05-31 00:06:42 +0600

    writting lines

--- Web development/Javascript/if-else statement.js
+++ Web development/Javascript/if-else statement.js
@@ -1,10 +1,17 @@
 let month= prompt("Enter the month name")
 
 
-if (month=="September"||"April"||"June"||"November") {
+if (month=="September"|| month=="April"|| month=="June" || month=="November") {
     alert("There are 30 days")
     
-} else{
-    alert("Not found")
+}
+else if(month=="February"){
+    alert("There are 28 days")
 
+}
+else if(month=="January"|| month=="March"|| month=="May" || month=="July"|| month=="August"|| month=="September"|| month=="October"|| month=="December"){
+    alert("There are 31 days")
+}
+else{
+    alert("Doesn't exist")
 }
